bd3dcd6095171f46724f41d0ed7aebd8cc51bf92
tommybautista <100245701+tommybautista@users.noreply.github.com>
2022-04-24 17:32:03 -0400

    init

---
+++ Python/flask/fundamentals/hello_flask/assignment/understanding-routing.py
@@ -0,0 +1,25 @@
+from distutils.log import debug
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<string:name>')
+def say(name):
+    print(name)
+    return 'Hi ' + name + '!'
+
+@app.route('/repeat/<int:num>/<string:word>')
+def repeat(num,word):
+    return word * num
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
