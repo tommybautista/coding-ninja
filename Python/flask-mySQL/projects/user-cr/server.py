70fe7e8185af218164c7427112e144bf37b6b0c8
tommybautista <100245701+tommybautista@users.noreply.github.com>
2022-05-02 14:03:00 -0400

    Update server.py

--- Python/flask-mySQL/projects/user-cr/server.py
+++ Python/flask-mySQL/projects/user-cr/server.py
@@ -1,14 +1,6 @@
 from flask_app import app
 from flask_app.controllers import controllers
-from friend import Friend
+from users import User
 
 if __name__=="__main__":
-    app.run(debug=True)
-
-
-
-
-
-            
-if __name__ == "__main__":
     app.run(debug=True)
