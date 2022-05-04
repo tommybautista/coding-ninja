8c5db4d23f5e0ec959b025dd8a1ec50508851735
tommybautista <100245701+tommybautista@users.noreply.github.com>
2022-05-04 18:19:05 -0400

    init

---
+++ Python/sql-flask/Users-CRUD/flask_app/models/user.py
@@ -0,0 +1,50 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class User:
+    db = 'user-cr'
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def getAll(cls):
+        query = 'SELECT * FROM user;'
+        results = connectToMySQL(cls.db).query_db(query)
+        users = []
+        for row in results:
+            users.append(cls(row))
+        return users
+
+    @classmethod
+    def getOne(cls, data):
+        query = 'SELECT * FROM user WHERE id = %()s;'
+        results =  connectToMySQL(cls.db).query_db(query, data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    @classmethod
+    def save(cls, data):
+        query = 'INSERT INTO user (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s);'
+        return connectToMySQL(cls.db).query_db(query, data)
+
+    @classmethod
+    def update(cls, data):
+        query = 'UPDATE user SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;'
+        return connectToMySQL(cls.db).query_db(query, data)
+
+    @classmethod
+    def delete(cls, data):
+        query = 'DELETE FROM user WHERE id = %(id)s;'
+        return connectToMySQL(cls.db).query_db(query, data)
+
+    @classmethod
+    def getCohort(cls, data):
+        # Here is where join statements will go to show all the cohorts for one subject
+        pass
+
+
