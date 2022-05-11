7e020d3aca93cf18fccb112cb11a37718ed969a6
tommybautista <100245701+tommybautista@users.noreply.github.com>
2022-05-11 18:56:21 -0400

    5/11

---
+++ Python/sql-flask/Dojo_Survey/flask_app/controllers/dojos.py
@@ -0,0 +1,23 @@
+from flask_app import app
+from flask import Flask, render_template, redirect, session, request
+from flask_app.models import dojo
+
+@app.route('/')
+def index():
+    return redirect('/dojos')
+
+@app.route('/dojos')
+def showDojos():
+    return render_template("dojos.html")
+
+
+@app.route('/dojos/submit', methods=["POST"])
+def createNinja():
+    if not dojo.Dojo.validate_ninja(request.form):
+        return redirect('/')
+    dojo.Dojo.save(request.form)
+    return redirect('/dojos/results')
+
+@app.results('dojos/results')
+def results():
+    return render_template("results.html")
---
+++ Python/sql-flask/Dojo_Survey/flask_app/models/dojo.py
@@ -0,0 +1,36 @@
+from select import select
+from flask_app.config import mysqlconnection
+from flask import flash
+
+class Dojo:
+    db = 'dojo_survey'
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.location = data['location']
+        self.language = data['language']
+        self.comment = data['comment']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def save(cls, data):
+        query = 'INSERT INTO dojos (name, location, language, comment) VALUES (%(name)s, %(location)s, %(language)s, %(comment)s);'
+        return mysqlconnection(cls.db).query_db(query, data)
+
+    @staticmethod
+    def validate_ninja(ninja):
+        is_valid = True # we assume this is true
+        if len(ninja['name']) < 3:
+            flash("Name must be at least 3 characters.")
+        #     is_valid = False
+        if ninja['location'] == 'Select a Location':
+            flash("Must select a Location")
+            is_valid = False
+        if (ninja['language']) == 'Select a Language':
+            flash("Must select a Language.")
+            is_valid = False
+        if len(ninja['comment']) < 1:
+            flash("Comment must be longer.")
+            is_valid = False
+        return is_valid
