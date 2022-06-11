0932eac1e13313f23e1c8091b97d28631dbc0566
tommybautista <100245701+tommybautista@users.noreply.github.com>
2022-06-11 13:18:25 -0400

    6-11

---
+++ JAVA/first_java_program/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Tommy");
+        System.out.println("I am 40 years old");
+        System.out.println("My hometown is Washington DC");
+    }
+}
