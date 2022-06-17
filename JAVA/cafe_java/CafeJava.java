53002520816c8f2d1911df4bc14c467f0fd5e4ac
tommybautista <100245701+tommybautista@users.noreply.github.com>
2022-06-17 15:48:14 -0400

    6-17

--- JAVA/cafe_java/CafeJava.java
+++ JAVA/cafe_java/CafeJava.java
@@ -1,52 +1,69 @@
 public class CafeJava {
+
     public static void main(String[] args) {
+
         // APP VARIABLES
+
         // Lines of text that will appear in the app. 
         String generalGreeting = "Welcome to Cafe Java, ";
         String pendingMessage = ", your order will be ready shortly";
         String readyMessage = ", your order is ready";
         String displayTotalMessage = "Your total is $";
+
         
         // Menu variables (add yours below)
         double mochaPrice = 3.5;
-        double dripCoffee = 4.1;
-        double latte = 3.6;
-        double cappucino = 5.2;
-    
+        double dripCoffeePrice = 2.0;
+        double lattePrice = 4.5;
+        double cappucinoPrice = 2.5;
+
         // Customer name variables (add yours below)
         String customer1 = "Cindhuri";
         String customer2 = "Sam";
         String customer3 = "Jimmy";
         String customer4 = "Noah";
-    
+
         // Order completions (add yours below)
         boolean isReadyOrder1 = false;
         boolean isReadyOrder2 = true;
         boolean isReadyOrder3 = false;
         boolean isReadyOrder4 = true;
-    
-        // APP INTERACTION SIMULATION (Add your code for the challenges below)
-        // Example:
-        System.out.println(generalGreeting + customer1 + pendingMessage); // Displays "Welcome to Cafe Java, Cindhuri"
+
+
+        // App interaction simulation (Add your code for the challenges below)
         
-        //Noah
-        if(isReadyOrder4 == true) {
-            System.out.println(customer4 + readyMessage + "\n" + displayTotalMessage + cappucino);
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        
+        // 1.) Cindhuri ordered a coffee. 
+        //     show her the status message.
+        System.out.println(customer1 + pendingMessage);
+
+        // 2.) Noah ordered a cappucino
+        //     Check the status of his order and let him know. If it is ready, also tell him his total.
+        if (isReadyOrder4) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + cappucinoPrice);
         }
         else {
-            System.out.println(customer4 +  pendingMessage);
+            System.out.println(customer4 + pendingMessage);
         }
-
-        //Sam
-        System.out.println(customer2 + ", " + displayTotalMessage + (latte*2));
-        if(isReadyOrder2 == true) {
+        
+        // 3.) Sam ordered 2 lattes, and would like to know the total up front. 
+        //      Also tell them their order status.
+        System.out.println(displayTotalMessage + lattePrice * 2);
+        if (isReadyOrder2) {
             System.out.println(customer2 + readyMessage);
         }
         else {
-            System.out.println(pendingMessage);
+            System.out.println(customer2 + pendingMessage);
         }
 
-        //Jimmy
-        System.err.println(customer3 + " " + "here is " + (dripCoffee - latte) + "back");
+
+        // 4.) Jimmy just realized he was charged for a coffee but had ordered a latte.
+        //     Tell him his new total to make up the difference.
+        System.out.println(displayTotalMessage + (lattePrice - dripCoffeePrice));
+
     }
+
 }
